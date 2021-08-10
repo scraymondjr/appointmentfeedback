d9e6d9a500b603c9c330eab28e38b77a0859af74
stephen.raymond <stephen.raymond@workday.com>
2021-08-09 21:59:10 -0600

    cleanup cli main

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/spf13/cobra"
-
 	"github.com/scraymondjr/appointment/cmd/cli/commander"
 	"github.com/scraymondjr/appointment/datastore/neo4j"
 )
-
-func init() {
-	root.AddCommand(ingest)
-}
 
 func main() {
 	store := neo4j.New()
@@ -20,18 +12,3 @@ func main() {
 		panic(err)
 	}
 }
-
-var (
-	root = &cobra.Command{
-		Run: func(cmd *cobra.Command, args []string) {
-
-		},
-	}
-	ingest = &cobra.Command{
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("ingest file" + args[0])
-		},
-		Use:  "ingest input_file.json",
-		Args: cobra.ExactArgs(1),
-	}
-)
